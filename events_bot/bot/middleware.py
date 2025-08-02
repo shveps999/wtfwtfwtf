54ac56eb3ea55ffa834a2f3c6e4a042f3cf68104
shveps999 <privetzdarovahappy@gmail.com>
2025-08-02 03:28:39 +0300

    Update middleware.py

--- events_bot/bot/middleware.py
+++ events_bot/bot/middleware.py
@@ -1,10 +1,9 @@
 from aiogram import BaseMiddleware
 from aiogram.types import TelegramObject
 from typing import Callable, Dict, Any, Awaitable
-from events_bot.database import get_async_session
+from events_bot.database import Database
 import logging
 
-# Настройка логирования
 logger = logging.getLogger(__name__)
 
 class DatabaseMiddleware(BaseMiddleware):
@@ -16,16 +15,6 @@ class DatabaseMiddleware(BaseMiddleware):
         event: TelegramObject,
         data: Dict[str, Any]
     ) -> Any:
-        # Используем асинхронный генератор для получения сессии
-        async for session in get_async_session():
+        async with Database.get_session() as session:
             data['db'] = session
-            try:
-                result = await handler(event, data)
-                await session.commit()
-                return result
-            except Exception as e:
-                logger.error(f"Ошибка в middleware: {e}")
-                await session.rollback()
-                raise
-            finally:
-                await session.close()
+            return await handler(event, data)
