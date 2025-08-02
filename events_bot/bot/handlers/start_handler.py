9918ab7dc31ff8210a8ed6cee7d9d2bdb0ee895a
shveps999 <privetzdarovahappy@gmail.com>
2025-08-02 03:38:45 +0300

    Update start_handler.py

--- events_bot/bot/handlers/start_handler.py
+++ events_bot/bot/handlers/start_handler.py
@@ -1,7 +1,6 @@
 from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
-from events_bot.database import Database
 from events_bot.database.services import UserService
 from events_bot.bot.states import UserStates
 from events_bot.bot.keyboards import get_city_keyboard, get_main_keyboard
@@ -12,31 +11,31 @@ def register_start_handlers(dp: Router):
     dp.include_router(router)
 
 @router.message(F.text == "/start")
-async def cmd_start(message: Message, state: FSMContext):
-    async with Database.get_session() as db:
-        # Регистрируем пользователя
-        user = await UserService.register_user(
-            db=db,
-            telegram_id=message.from_user.id,
-            username=message.from_user.username,
-            first_name=message.from_user.first_name,
-            last_name=message.from_user.last_name,
-        )
+async def cmd_start(message: Message, state: FSMContext, db):
+    """Обработчик команды /start"""
+    # Регистрируем пользователя
+    user = await UserService.register_user(
+        db=db,
+        telegram_id=message.from_user.id,
+        username=message.from_user.username,
+        first_name=message.from_user.first_name,
+        last_name=message.from_user.last_name,
+    )
 
-        # Проверяем, есть ли у пользователя город
-        if not user.city:
-            await message.answer(
-                "👋 Добро пожаловать!\n\n"
-                "Для начала работы выберите ваш город:",
-                reply_markup=get_city_keyboard(),
-            )
-            await state.set_state(UserStates.waiting_for_city)
-        else:
-            welcome_text = f"👋 С возвращением, {user.first_name or user.username or 'пользователь'}!\n\n"
-            welcome_text += "Выберите действие"
-            
-            await message.answer(
-                welcome_text,
-                reply_markup=get_main_keyboard(),
-                parse_mode="Markdown"
-            )
+    # Проверяем, есть ли у пользователя город
+    if not user.city:
+        await message.answer(
+            "👋 Добро пожаловать в Сердце!\n\n"
+            "Для начала работы выберите ваш город:",
+            reply_markup=get_city_keyboard(),
+        )
+        await state.set_state(UserStates.waiting_for_city)
+    else:
+        welcome_text = f"👋 С возвращением, {user.first_name or user.username or 'пользователь'}!\n\n"
+        welcome_text += "Выберите действие"
+        
+        await message.answer(
+            welcome_text,
+            reply_markup=get_main_keyboard(),
+            parse_mode="Markdown"
+        )
