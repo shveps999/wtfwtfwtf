f860cd4563561b0291be4713f508837804ad05e7
shveps999 <privetzdarovahappy@gmail.com>
2025-08-07 17:22:23 +0300

    Delete events_bot/bot/states directory

--- events_bot/bot/states/__init__.py
+++
@@ -1,4 +0,0 @@
-from .user_states import UserStates
-from .post_states import PostStates
-
-__all__ = ["UserStates", "PostStates"]
--- events_bot/bot/states/user_states.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-
-class UserStates(StatesGroup):
-    """Состояния для регистрации пользователя"""
-
-    waiting_for_city = State()
-    waiting_for_categories = State()
