8d227aa3f05f5c0f4ae68bd413b2478da44f4113
shveps999 <privetzdarovahappy@gmail.com>
2025-08-20 19:39:47 +0300

    Add files via upload

---
+++ events_bot/database/services/__init__.py
@@ -0,0 +1,15 @@
+from .user_service import UserService
+from .category_service import CategoryService
+from .post_service import PostService
+from .notification_service import NotificationService
+from .moderation_service import ModerationService
+from .like_service import LikeService
+
+__all__ = [
+    "UserService",
+    "CategoryService",
+    "PostService",
+    "NotificationService",
+    "ModerationService",
+    "LikeService",
+]
