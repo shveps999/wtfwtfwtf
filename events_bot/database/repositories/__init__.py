3ee15d6839a7b58a7a5d36bf46656a25eeba88ff
shveps999 <privetzdarovahappy@gmail.com>
2025-08-20 19:38:55 +0300

    Add files via upload

---
+++ events_bot/database/repositories/__init__.py
@@ -0,0 +1,13 @@
+from .user_repository import UserRepository
+from .category_repository import CategoryRepository
+from .post_repository import PostRepository
+from .moderation_repository import ModerationRepository
+from .like_repository import LikeRepository
+
+__all__ = [
+    "UserRepository",
+    "CategoryRepository", 
+    "PostRepository",
+    "ModerationRepository",
+    "LikeRepository",
+]
